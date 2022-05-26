24a94a0df0af3d6bf47b879e5f89aab96bcd7cb2
Sheni Hamitaj <shen.i@live.com>
2022-05-26 11:13:06 +0200

    --add: icons to services

--- resources/subpages/services.js
+++ resources/subpages/services.js
@@ -6,18 +6,22 @@ export default {
   products: [
     {
       title: 'Mobile Apps',
+      icon: 'https://shemehamitaj.com/assets/images/mobileapp.svg',
       description: 'Developing memorable and unique mobile Android and IOS apps.'
     },
     {
       title: 'Desktop Apps',
+      icon: 'https://shemehamitaj.com/assets/images/desktopapp.svg',
       description: 'Delivering high-end and secure desktop apps for Windows/Linux OS.'
     },
     {
       title: 'Web Apps',
+      icon: 'https://shemehamitaj.com/assets/images/webapp.svg',
       description: 'Sleek and fully-responsive websites for all kinds of screens and devices.'
     },
     {
       title: 'Graphic Design',
+      icon: 'https://shemehamitaj.com/assets/images/graphicdesign.svg',
       description: 'Turning ideas into perfect graphical contents for your business.'
     },
   ]
