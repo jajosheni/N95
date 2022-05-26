04edec29a2463b89c15b538a23222e08a42587b8
Sheni Hamitaj <shen.i@live.com>
2022-05-26 15:12:48 +0200

    --add: button and card shared components

---
+++ src/shared/Button.svelte
@@ -0,0 +1,44 @@
+<script>
+  export let type = 'primary';
+  export let flat = false;
+  export let inverse = false;
+</script>
+
+<button class={type} class:flat={flat} class:inverse={inverse} on:click>
+    <slot/>
+</button>
+
+<style>
+    button {
+        border: 0;
+        cursor: pointer;
+        border-radius: 6px;
+        padding: 8px 12px;
+        font-weight: bold;
+        box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.2);
+    }
+
+    .primary {
+        background: #d91b42;
+        color: white;
+    }
+
+    .secondary {
+        background: #45c496;
+        color: white;
+    }
+
+    .flat {
+        box-shadow: none;
+    }
+
+    .primary.inverse {
+        background: white;
+        color: #d91b42;
+    }
+
+    .secondary.inverse {
+        background: white;
+        color: #45c496;
+    }
+</style>
---
+++ src/shared/Card.svelte
@@ -0,0 +1,12 @@
+<div class="card">
+    <slot></slot>
+</div>
+
+<style>
+    .card {
+        background: white;
+        padding: 20px;
+        border-radius: 6px;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+    }
+</style>
