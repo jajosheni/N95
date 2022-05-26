a44fbf54839a78dc02ffab857c0383620af4f6cd
Sheni Hamitaj <shen.i@live.com>
2022-05-26 16:59:12 +0200

    --add: drawer card

---
+++ src/components/Drawer.svelte
@@ -0,0 +1,23 @@
+<script>
+
+</script>
+
+<div class="grid-item-drawer">
+
+</div>
+
+<style>
+    .grid-item-drawer {
+        grid-area: drawer;
+        position: relative;
+        background: var(--card-background);
+        width: 100%;
+        height: 67vh;
+        box-shadow: 3px 0 10px rgba(0, 0, 0, 0.2) inset;
+        border-radius: 0 var(--radius) var(--radius) 0;
+        overflow-y: scroll;
+        margin-top: 20px;
+        margin-bottom: 10px;
+        padding-bottom: 10px;
+    }
+</style>
