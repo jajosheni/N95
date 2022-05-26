3dfd7e193e733f734fd033b4c1ccf631316e57be
Sheni Hamitaj <shen.i@live.com>
2022-05-26 12:31:14 +0200

    --add: bg animation

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,21 @@
 <script>
+	import bgAnimation from './styles/bgAnimation';
 </script>
 
+<header>
+	<div class="animation-area">
+		<ul class="box-area">
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+		</ul>
+	</div>
+</header>
 <main>
 	<h1>Hello!</h1>
 </main>
 
-<style>
-</style>
+{@html `<style>${bgAnimation}</style>`}
