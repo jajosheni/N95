6b1218872eec4bd9a1fd4dd48e284caf24033d8b
Sheni Hamitaj <shen.i@live.com>
2022-05-26 15:23:30 +0200

    --add: miscellaneous

---
+++ resources/miscellaneous.js
@@ -0,0 +1,11 @@
+export const title = 'Sheme Hamitaj | Personal Page';
+export const meta = [
+  {
+    name: 'description',
+    content: 'BSc. Sheme Hamitaj is a Computer Engineer, Photographer & Designer based in Albania.'
+  },
+  {
+    name: 'keywords',
+    content: 'sheme,hamitaj,personal,site,developer,software,web,desktop,mobile,fast,reliable,resume,tirana,albania,github,pages,shqiperi,faqe,personale,tirane,zhvillues,programues,programe,profesional,sherbim'
+  }
+];
