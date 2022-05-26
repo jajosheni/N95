6cad4b4e60bd1943d0e43cf85cbc66b855a6a428
Sheni Hamitaj <shen.i@live.com>
2022-05-26 14:18:40 +0200

    --add: header button

---
+++ src/shared/HeaderButton.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let name;
+  export let image;
+</script>
+
+<div class="header-button">{name}</div>
+
+<style>
+    .header-button {
+        text-transform: uppercase;
+        text-align: center;
+        font-size: 0.75em;
+        font-weight: 500;
+        color: var(--text)
+    }
+</style>
