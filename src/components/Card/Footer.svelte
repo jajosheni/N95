3b414f88e407a3d35db1e942c0552d64957e4a0d
Sheni Hamitaj <shen.i@live.com>
2022-05-26 18:00:25 +0200

    --add: init footer and social media components

---
+++ src/components/Card/Footer.svelte
@@ -0,0 +1,11 @@
+<script></script>
+
+
+<div class="bottom-buttons">
+    <button onclick="downloadCV();" class="btn download-cv">CV<span class="icon"></span></button>
+    <div class="splitter"></div>
+    <button onclick="sendMail();" class="btn email-me">e-mail<span class="icon"></span></button>
+</div>
+
+
+<style></style>
---
+++ src/components/Card/SocialMedia.svelte
@@ -0,0 +1,12 @@
+<script></script>
+
+
+
+<ul class="social-media">
+    <li onclick="window.open('https://github.com/jajosheni', '_blank');" class="github"></li>
+    <li onclick="window.open('https://linkedin.com/in/shemehamitaj', '_blank');" class="linkedin"></li>
+    <li onclick="window.open('https://play.google.com/store/apps/dev?id=9083946006672030470', '_blank');"
+        class="apps"></li>
+</ul>
+
+<style></style>
