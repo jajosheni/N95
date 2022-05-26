6771afe69dcd5a26482cd023ff7b89c45f95b4ff
Sheni Hamitaj <shen.i@live.com>
2022-05-26 16:54:54 +0200

    --add: profile card

---
+++ src/components/Profile.svelte
@@ -0,0 +1,20 @@
+<script>
+
+</script>
+
+<div class="grid-item-profile">
+
+</div>
+
+<style>
+    .grid-item-profile {
+        grid-area: profile;
+        height: 72vh;
+        min-height: 450px;
+        box-shadow: 0 -5px 10px rgba(0, 0, 0, 0.2);
+        border-radius: var(--radius);
+        position: relative;
+        background: var(--card-background);
+        width: 100%
+    }
+</style>
