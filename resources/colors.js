2ae859d7b696111a6f8fa37210ef99ce7e9e1d89
Sheni Hamitaj <shen.i@live.com>
2022-05-26 11:39:48 +0200

    --refac: put font in colors

--- resources/colors.js
+++ resources/colors.js
@@ -1,4 +1,5 @@
 export default {
+  fontUrl: 'https://fonts.googleapis.com/css2?family=Poppins',
   primary: '#5196fd',
   background: '#31313a',
   darkerBackground: '#1c1c21',
--- resources/miscellaneous.js
+++
@@ -1,5 +0,0 @@
-export default {
-  fontUrl: 'https://fonts.googleapis.com/css2?family=Poppins',
-  metaDescription: 'BSc. Sheme Hamitaj is a Computer Engineer, Photographer & Designer based in Albania.',
-  metaKeywords: 'sheme,hamitaj,personal,site,developer,web,desktop,mobile,fast,reliable,resume,tirana,albania,shqiperi,faqe,personale,tirane,zhvillues,programues,profesional,sherbim,software',
-};
