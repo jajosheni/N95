033772db0d7f546f47fa15b5a70f7ede65888b0e
Sheni Hamitaj <shen.i@live.com>
2022-05-27 13:50:55 +0200

    --refac: extract quote to component

---
+++ src/components/pages/components/Quote.svelte
@@ -0,0 +1,23 @@
+<script>
+  export let quote;
+</script>
+
+<div class="quote">
+    <span class="left mt-2" style="margin-left: -10px">"</span>
+    <blockquote>{quote}</blockquote>
+    <span class="right mt-3 mr-1">"</span>
+</div>
+
+<style>
+    .quote {
+        text-align: center;
+        margin: 20px auto;
+    }
+
+    .quote > span {
+        color: var(--primary);
+        font-size: 4em;
+        font-style: italic;
+        line-height: 0
+    }
+</style>
