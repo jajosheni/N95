7aafe38b71abfd7257c19c88c2d9ce5ee92805da
Sheni Hamitaj <shen.i@live.com>
2022-05-27 17:12:28 +0200

    --add: section component

---
+++ src/components/pages/Resume/Section.svelte
@@ -0,0 +1,21 @@
+<script>
+  export let item = {
+    title: '',
+    icon: ''
+  };
+</script>
+
+<div>
+    <div class="section-title">
+        <img class="s-50 mr-1" src={item.icon} alt={item.title}>
+        <span class="uppercase">{item.title}</span>
+    </div>
+    <slot></slot>
+</div>
+
+<style>
+    .section-title > img,
+    .section-title > span {
+        vertical-align: middle;
+    }
+</style>
