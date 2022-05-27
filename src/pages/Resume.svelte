4b147ba587b61f1568a2cba903c84b91b02044a6
Sheni Hamitaj <shen.i@live.com>
2022-05-27 17:12:38 +0200

    --add: experiences to resume

---
+++ src/pages/Resume.svelte
@@ -0,0 +1,32 @@
+<script>
+  import resume from '../../resources/subpages/resume';
+  import Section from '../components/pages/Resume/Section.svelte';
+  import Card from '../shared/Card.svelte';
+  import Experience from '../components/pages/Resume/Experience.svelte';
+</script>
+
+<div class="resume-container">
+    <div class="section">
+        <Card>
+            <Section item={resume.experience}>
+                <div>
+                    {#each resume.experience.data as experience}
+                        <Experience {experience}/>
+                    {/each}
+                </div>
+            </Section>
+        </Card>
+    </div>
+</div>
+
+<style>
+    .section {
+        width: 49.5%;
+    }
+
+    .resume-container {
+        display: flex;
+        justify-content: space-between;
+        flex-wrap: wrap;
+    }
+</style>
