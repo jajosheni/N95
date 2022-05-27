5834893b543486a6af2684d5b48ede1da866c003
Sheni Hamitaj <shen.i@live.com>
2022-05-27 14:06:54 +0200

    --add: hobby component

---
+++ src/components/pages/components/Hobby.svelte
@@ -0,0 +1,30 @@
+<script>
+  import Card from '../../../shared/Card.svelte';
+
+  export let hobby = {
+    title: '',
+    image: '',
+    description: ''
+  };
+</script>
+
+<div class="mb-1">
+<Card>
+    <div class="hobby-container">
+        <img class="icon mr-1" src={hobby.image} alt={hobby.title}>
+        {hobby.title} - {hobby.description}
+    </div>
+</Card>
+</div>
+
+<style>
+    .hobby-container {
+        display: flex;
+        justify-content: space-between;
+    }
+
+    .icon {
+        width: 50px;
+        height: 50px;
+    }
+</style>
