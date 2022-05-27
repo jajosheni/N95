ea01e1aecbc75c0fc96b0182f5b42044654bf1f9
Sheni Hamitaj <shen.i@live.com>
2022-05-27 13:37:24 +0200

    --add: field components

---
+++ src/components/pages/components/Field.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let field;
+</script>
+
+<div class="field">
+    <span class="mr-2">{field.name}:</span>
+    <span class="right">{field.value}</span>
+</div>
+
+<style>
+    .field {
+        min-width: 200px;
+    }
+</style>
---
+++ src/components/pages/components/Fields.svelte
@@ -0,0 +1,28 @@
+<script>
+  import Field from './Field.svelte';
+
+  export let fields;
+</script>
+
+<div class="fields">
+    {#each fields as field, i}
+        {#if i % 2 === 0}
+            <div>
+                <Field {field}/>
+                <Field field={fields[i + 1]}/>
+            </div>
+        {/if}
+    {/each}
+</div>
+
+<style>
+    .fields {
+        width: 100%;
+        text-transform: uppercase;
+        font-size: 0.9em;
+        display: flex;
+        justify-content: space-between;
+        align-items: flex-start;
+    }
+
+</style>
