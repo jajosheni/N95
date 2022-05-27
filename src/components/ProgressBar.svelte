9511b6f4aaa360193c6e6f6112c81dc126faef89
Sheni Hamitaj <shen.i@live.com>
2022-05-27 18:19:48 +0200

    --add: progress bar line

---
+++ src/components/ProgressBar.svelte
@@ -0,0 +1,29 @@
+<script>
+  export let type = 'line';
+  export let level = 0;
+
+  $: percentage = (level < 0 || level > 100) ? 50 : level;
+</script>
+
+{#if type === 'line'}
+    <div class="progress mt-1">
+        <div class="percentage" style="width:{percentage}%;"></div>
+    </div>
+{:else if type === 'dots'}
+    <div class="dots mt-1">
+        <div class="dots-percentage" style="width:{percentage}%;"></div>
+    </div>
+{/if}
+
+<style>
+    .progress, .percentage {
+        width: 100%;
+        height: var(--radius);
+        border-radius: var(--radius);
+        background-color: var(--background);
+    }
+
+    .percentage {
+        background-color: var(--primary);
+    }
+</style>
