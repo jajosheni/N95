130bf190d2c2cd2e802cdbf010ef1b8970e7bff0
Sheni Hamitaj <shen.i@live.com>
2022-05-27 15:10:31 +0200

    --add: product component

---
+++ src/components/pages/components/Product.svelte
@@ -0,0 +1,25 @@
+<script>
+  import Card from '../../../shared/Card.svelte';
+
+  export let product = {
+    title: '',
+    icon: '',
+    description: ''
+  };
+</script>
+
+<Card>
+    <div class="product-title mb-2">
+        <img class="s-50 mr-1" src={product.icon} alt={product.title}>
+        <span class="uppercase">{product.title}</span>
+    </div>
+    <span>{product.description}</span>
+</Card>
+
+<style>
+    .product-title > img,
+    .product-title > span {
+        vertical-align: middle;
+    }
+</style>
+
