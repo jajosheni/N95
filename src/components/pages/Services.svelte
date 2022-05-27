1113b3d26fd27e6204eab138673f1a04f2f96ac3
Sheni Hamitaj <shen.i@live.com>
2022-05-27 15:10:39 +0200

    --refac: add services page

---
+++ src/components/pages/Services.svelte
@@ -0,0 +1,26 @@
+<script>
+  import services from '../../../resources/subpages/services';
+  import Product from './components/Product.svelte';
+</script>
+
+
+<span class="block bold mb-2">Fast and Reliable.</span>
+<div class="services-container">
+    {#each services.products as product}
+        <div class="product mb-3">
+            <Product {product}/>
+        </div>
+    {/each}
+</div>
+
+<style>
+    .product {
+        width: 47.5%;
+    }
+
+    .services-container {
+        display: flex;
+        justify-content: space-between;
+        flex-wrap: wrap;
+    }
+</style>
