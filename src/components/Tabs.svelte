9e423e955ed2ca13fed8aec68ccb6869631b8f1a
Sheni Hamitaj <shen.i@live.com>
2022-05-28 14:22:32 +0200

    --add: auto scroll on tab change

--- src/components/Tabs.svelte
+++ src/components/Tabs.svelte
@@ -4,12 +4,22 @@
   export let items = [];
 
   const dispatch = createEventDispatcher();
+
+  const handleClick = (tabName) => {
+    dispatch('tabChange', tabName);
+
+    document.getElementById('drawer-anchor').scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  };
+
 </script>
 
 <div class="tabs">
     <ul class="uppercase">
         {#each items as item (item.name)}
-            <li class:active={item.active} on:click={() => dispatch('tabChange', item.name)}>
+            <li class:active={item.active} on:click={() => handleClick(item.name)}>
                 <i class="mdi mdi-24px {item.icon} text-color"></i>
                 <span>{item.pageTitle}</span>
             </li>
