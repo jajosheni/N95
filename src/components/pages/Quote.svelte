ad7bcb3f7502d9a657e7c60d83d3c5f8be015284
Sheni Hamitaj <shen.i@live.com>
2022-05-28 13:27:27 +0200

    --fix: quote color

--- src/components/pages/Quote.svelte
+++ src/components/pages/Quote.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="quote">
-    <span class="left mt-2" style="margin-left: -10px">"</span>
+    <span class="left mt-2 primary-color" style="margin-left: -10px">"</span>
     <blockquote>{quote}</blockquote>
     <span class="right mt-3 mr-1 primary-color">"</span>
 </div>
